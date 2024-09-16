9404b40df63b1f04c692d59889527aa2a02c3dbb
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-09-16 19:13:01 +0700

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
