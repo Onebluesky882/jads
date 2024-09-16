2f137fd3e24bc62c57a72d8435b82325bc5ef3a3
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-09-16 19:11:10 +0700

    setup

---
+++ src/Pages/Homepage.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const Homepage = () => {
+  const showTime = () => {
+    const time = new Date().toISOString();
+    return time;
+  };
+
+  useEffect(() => {
+    showTime();
+  });
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+export default Homepage;
